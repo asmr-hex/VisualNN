5d860285e9d608e05ccc0654b75dfc9fb0fb480c
connorwalsh <connorw@andrew.cmu.edu>
2015-08-13 14:14:36 -0400

    Implemented learning. Needs cleaning.

--- VisualNN.py
+++ VisualNN.py
@@ -17,15 +17,21 @@ class NeuralNetwork(object):
 	"""
 	
 	__Layers = {}	#Neuronal layers
-	__Weights = []	#Network Weights
 	__illustrator = None	#Illustrator instance
-	def __init__(self, architecture):
+	Error = 0	#Output error
+	
+	def __init__(self, architecture, f, df, verbose=False):
 		"""Inits NeuralNetwork with architecture."""
+		self.__verbose = verbose
 		self.__buildArchitecture(architecture)
+		self.__initializeWeights()
+		self.__setActivationFunctions(f,df)
 		self.__illustrator = Illustrator(self.__Layers)
 		
 	def __buildArchitecture(self, architecture):
 		"""Parses architecture string and builds network."""
+		if self.__verbose:
+			print "Building NN Architecture..."
 		L_GROUP = 1	#Layer group for regex
 		S_GROUP = 4	#NodeID start group for regex
 		E_GROUP = 5	#NodeID end group for regex
@@ -46,9 +52,13 @@ class NeuralNetwork(object):
 					src_neuron.addOutput(dest_neurons)	#Add dests to src
 				for dest_neuron in dest_neurons:
 					dest_neuron.addInput(src_neurons)	#Add src to dests
+	
+	def illustrate(self):
+		"""Illustrate network design."""
+		self.__illustrator.draw()
 					
 	def __parseRange(self, start, end):
-		"""Helper function to parse ranges for architecture setup"""
+		"""Helper function to parse ranges for architecture setup."""
 		end = int(end)
 		if start is None:
 			start = end
@@ -57,7 +67,7 @@ class NeuralNetwork(object):
 		return range(start, end+1)
 		
 	def __getNeurons(self, layer, nids):
-		"""Helper function to find neuron, or return a new one"""
+		"""Helper function to find neuron, or return a new one."""
 		neurons = []
 		for nid in nids:
 			layers = self.__Layers.keys()
@@ -74,16 +84,126 @@ class NeuralNetwork(object):
 					self.__Layers[layer].append(neuron)
 					neurons.append(neuron)
 		return neurons
-				
+	
+	def __initializeWeights(self):
+		"""Initialize weights and biases."""
+		if self.__verbose:
+			print "Initializing NN Weights and Biases..."
+		for L in xrange(0,len(self.__Layers)-1):
+			#Initialize one bias for each layer
+			bias = Neuron(L, len(self.__Layers[L]), bias=True)
+			bias.addOutput(self.__Layers[L+1])
+			for neuron in self.__Layers[L+1]:
+				neuron.addInput([bias])
+			#Initialize weights for this layer
+			self.__Layers[L].append(bias)
+			for neuron in self.__Layers[L]:
+				outward = neuron.getOutput()
+				for next_neuron in outward:
+					neuron.w[next_neuron.getID()] = np.random.rand(1)
+	
+	def __setActivationFunctions(self, f, df):
+		"""Assign Activation functions and derivatives to neurons."""
+		#Handle case if there are multiple types of Activations
+		if type(f) is list and type(df) is list:
+			activation_type = 'multiple'
+			if len(f) != len(self.__Layers) or len(df) != len(self.__Layers):
+				raise ValueError('number of activations and layers must equal!')
+		else:
+			activation_type = 'single'
+		for L in self.__Layers.keys():
+			#Label layer type
+			if L == 0:
+				layer_type = "input"
+			elif L == len(self.__Layers.keys())-1:
+				layer_type = "output"
+			else:
+				layer_type = "hidden"
+			#Select function
+			if activation_type is 'single':
+				F = f
+				dF = df
+			else:
+				F = f[L]
+				dF = df[L]
+			for neuron in self.__Layers[L]:
+				neuron.setLayerType(layer_type)
+				neuron.setFunctions(F, dF)
 		
+	def getOutput(self):
+		"""Get output layer of network."""
+		L = len(self.__Layers) - 1
+		output = np.empty([1,len(self.__Layers[L])])
+		for idx,neuron in enumerate(self.__Layers[L]):
+			output[0,idx] = neuron.A
+		return output
 		
 	def feedForward(self, x):
 		"""Feeds network inputs, stores intermediate values, returns output."""
-		pass
+		if self.__verbose:
+			print "Feeding Forward..."
+		#Ensure that input size matches inputs
+		if len(x) != len(self.__Layers[0])-1:
+			raise ValueError("incorrect network input size!")
+		for L in self.__Layers.keys():
+			idx = 0
+			for neuron in self.__Layers[L]:
+				#Set input neurons to input data
+				if neuron.layer_type is "input":
+					if neuron.bias:		#Don't set biases in input layer
+						continue
+					else:
+						neuron.A = x[idx]
+						idx+=1
+						continue
+				#Compute Activations for each neuron
+				if neuron.bias:
+					continue
+				else:
+					for inputs in neuron.getInput():
+						neuron.Sigma += inputs.w[neuron.getID()] * inputs.A
+					neuron.computeActivation(neuron.Sigma)
 		
-	def backPropagation(self):
+	def backPropagation(self, y):
 		"""Performs backpropogation of errors over the neural network."""
-		pass
+		if self.__verbose:
+			print "backpropagating errors..."
+		self.computeError(y)
+		L = len(self.__Layers.keys()) - 1
+		#Propogate error signal backwards through the network
+		for Lidx in xrange(L, 0, -1):
+			layer = self.__Layers[Lidx]
+			for idx, neuron in enumerate(layer):
+				if neuron.layer_type is 'output':
+					neuron.dError = self.computeErrorPrime(y[idx], neuron.A)
+				else:
+					neuron.dError = 0	#Clear error signal for SGD (TODO: support Batch training?)
+					for outputs in neuron.getOutput():
+						neuron.dError += outputs.dError * neuron.w[outputs.getID()]
+						
+	def updateWeights(self, rate, momentum=1):
+		"""Update the weights based on the propogated error signal."""
+		if self.__verbose:
+			print "updating weights..."
+		for L in self.__Layers.keys():
+			for neuron in self.__Layers[L]:
+				outputs = neuron.getOutput()
+				#update weights
+				for out in outputs:
+					delta = rate * out.dError * out.dF(out.Sigma) * neuron.A
+					neuron.w[out.getID()] +=  delta
+						
+		
+	def computeError(self, y):
+		"""Compute error signal from outputs."""
+		o = self.getOutput()
+		if len(o) != len(y):
+			raise ValueError("Size mismatch between labels and outputs!")
+		self.Error = 0.5 * np.sum([(y[i]-o[i])**2 for i in range(0,len(y))])
+		
+	def computeErrorPrime(self, yi, ai):
+		"""Compute the derivative of the error signal."""
+		return -(yi-ai)
 		
 		
 class Neuron(object):
@@ -93,29 +213,47 @@ class Neuron(object):
 	storage for activations achieved during forward passes, and
 	storage for error signals during backwards propogation. """
 	
-	__f = ""	#Activation function
-	__A = 0	#Current activation
+	__f = None	#Activation function
+	__df = None	#Derivative of Activation function
+	A = 0.	#Current activation
+	dError = 0	#Propogated error signal
+	Sigma = 0.	#Current Summation
+	bias = False	#is this a bias node?
 	__inputs = []	#list of input neurons
 	__outputs = []	#list of output neurons
+	w = {}	#Dictionary of weights corresponding to outputs
 	__layer = None
 	__id = None
-	__x = 0	# graphical x positioning
-	__y = 0	# graphical y positioning
+	x = 0	# graphical x positioning
+	y = 0	# graphical y positioning
 	__r = 0 # graphical radius
 	__c = 0	# graphical color
 	__graphic = None	#graphical representation
-	def __init__(self, layer, nid):
+	def __init__(self, layer, nid, bias=False):
 		"""Inits Neuron with activation function."""
 		self.__layer = layer
 		self.__id = nid
+		if bias:
+			self.bias = True
+			self.A = 1
+			self.Sigma = 1
+	
+	def setLayerType(self, layer_type):
+		"""Set the type of layer this neuron resides in."""
+		self.layer_type = layer_type
+		
+	def setFunctions(self, f, df):
+		"""Sets activation function and derivative."""
+		self.__f = f
+		self.__df = df
 		
 	def computeActivation(self, x):
 		"""Uses activation function to compute activation."""
-		pass
-	
-	def getActivation(self):
-		"""Gets current activation from neuron."""
-		return self.__A
+		self.A = self.__f(x)
+		
+	def dF(self, x):
+		"""Computes the derivative of the activation function."""
+		return self.__df(x)
 		
 	def addOutput(self, neurons):
 		"""Adds output connectivity between this and other neurons."""
@@ -135,6 +273,10 @@ class Neuron(object):
 		"""Gets output connected neurons."""
 		return self.__outputs
 		
+	def getInput(self):
+		"""Gets input connected neurons."""
+		return self.__inputs
+		
 	def getID(self):
 		"""Get the neuron id of this neuron."""
 		return self.__id
@@ -145,8 +287,8 @@ class Neuron(object):
 		
 	def setXYR(self, x, y, r):
 		"""Sets the graphical x,y positions."""
-		self.__x = x
-		self.__y = y
+		self.x = x
+		self.y = y
 		self.__r = r
 		
 	def setColor(self, color):
@@ -155,7 +297,7 @@ class Neuron(object):
 		
 	def getGraphic(self):
 		"""Returns graphical object for this neuron."""
-		circle = plt.Circle((self.__x, self.__y),
+		circle = plt.Circle((self.x, self.y),
 		 			radius=self.__r, color=self.__c, zorder=2,
 					clip_on=False)
 		return circle
@@ -180,6 +322,12 @@ class Illustrator(object):
 	def __init__(self, layers):
 		"""Inits visualization tool for a NeuralNetwork instance."""
 		self.__configureArchitecture(layers)
+		#self.draw()
+		
+		#self.__neurons[0][0].setColor("#F3CA7E")
+		#plt.pause(1)   
+		#self.draw() 
+		
 		
 	def __configureArchitecture(self, layers):
 		"""Configures the layout of the graphical network model."""
@@ -199,25 +347,29 @@ class Illustrator(object):
 				y = n*height + (height/2)
 				layers[l][n].setXYR(x,y, self.__radii)
 				layers[l][n].setColor('#2481F3')
-				self.__neurons[l].append(layers[l][n].getGraphic())
-				self.__ax.add_patch(self.__neurons[l][n])
-				edges = layers[l][n].getOutput()
-				for e in xrange(0, len(edges)):
-					dest_l = edges[e].getLayer()
-					dest_height = (self.__ax_ylim)/(len(layers[dest_l]))
-					edgex = [x, self.__layer_x[dest_l]]
-					edgey = [y, e*dest_height + (dest_height/2)]
-					edgeStr = 'L'+str(l)+'N'+str(n)+'->'
-					edgeStr+= 'L'+str(dest_l)+'N'+str(edges[e].getID())
-					self.__edges[edgeStr] = [[x,y],[edgex[1],edgey[1]]]
-					self.__ax.plot(edgex, edgey, color='k', zorder=1)
+				self.__neurons[l].append(layers[l][n])
+		
+	def setLayerColors(self, colors):
+		"""Set colors of each layer."""
+		
+		
+	def draw(self):
+		"""Draws the current NeuralNetwork instance state."""
+		plt.ion()
+		plt.cla()
+		for layer in self.__neurons.keys():
+			for neuron in self.__neurons[layer]:
+				self.__ax.add_patch(neuron.getGraphic())
+				edges = neuron.getOutput()
+				for next_neuron in edges:
+					x = [neuron.x, next_neuron.x]
+					y = [neuron.y, next_neuron.y]
+					self.__ax.plot(x, y, color='k', zorder=1)
 		self.__ax.set_ylim(0,self.__ax_ylim)
 		self.__ax.set_xlim(0,self.__ax_xlim)
 		self.__ax.axis('scaled')
 		self.__ax.axis('Off')
-		plt.show()
-		
-	def draw(self):
-		"""Draws the current NeuralNetwork instance state."""
-		pass
+		plt.draw()
+		plt.show(block=True)
+					
 		
--- example.py
+++ example.py
@@ -1,4 +1,19 @@
 from VisualNN import *
+import numpy as np
 
-NN = NeuralNetwork('(0,0:3)->(1,0:2)->(2,0:15)->(3,0:1)')
-#NN1 = NeuralNetwork('(0,0:5)->(1,0:3)->(2,0:8)->(3,0:2)')
+arch = '(0,0:1)->(1,0:1)->(2,0)'
+#f = lambda X: X
+
+#arch = '(0,0:3)->(1,0:2)->(2,0:15)->(3,0:1)'
+f = lambda X: 1./(1. + np.exp(-X))
+df = lambda X: np.exp(-X)/(1. + np.exp(-X))
+
+NN = NeuralNetwork(arch, f, df, verbose=True)
+NN.feedForward(np.array([0.5,0.7]))
+print NN.getOutput()
+NN.backPropagation(np.array([3.]))
+NN.updateWeights(.1)
+NN.feedForward(np.array([0.5,0.7]))
+print NN.getOutput()
+
+#NN.illustrate()
